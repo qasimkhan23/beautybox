03a64289ba610c2ff786f40fddbb54934d4a82cb
Aruba <arubakhan23@gmail.com>
2019-02-03 13:21:21 +0500

    pages new

---
+++ src/Pages/Gallery/Gallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gallery = () =>{
+    return(
+        <div>
+         Hellow word
+
+         </div>
+    )
+}
+export default Gallery;
---
+++ src/Pages/Signout/Signout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Signout = () =>{
+    return(
+        <div>
+         Hellow word
+
+         </div>
+    )
+}
+export default Signout;
+
