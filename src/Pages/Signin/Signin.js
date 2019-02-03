719df40f4ac2e8d6a5549e509431ae5ce57091d7
Qasim Khan <kqasim109@gmail.com>
2019-02-03 21:03:28 +0500

    sign in

--- src/Pages/Signin/Signin.js
+++ src/Pages/Signin/Signin.js
@@ -4,28 +4,46 @@ import { withStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Header from "../../components/Header/header";
-const styles = theme => ({
-  root: {
-    ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2
-  }
-});
+import TextField from "@material-ui/core/TextField";
+import { MDBInput } from "mdbreact";
+import { MDBBtn } from "mdbreact";
+
+import Card from "@material-ui/core/Card";
+
+const styles = {};
 const Signin = props => {
   const { classes } = props;
   return (
     <div>
       <Header />
       <div className="container">
-        <Paper className={classes.root} elevation={1}>
+        <Card
+          style={{
+            margin: 100,
+            padding: 100
+          }}
+        >
           <Typography variant="h5" component="h3">
-            This is a sheet of paper.
+            SignIn
           </Typography>
-          <Typography component="p">
-            Paper can be used to build surface or other elements for your
-            application.
-          </Typography>
-        </Paper>
+          <div className="row">
+            <div className="col-md-12">
+              <MDBInput label="Username" icon="user" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <MDBInput label="Password" icon="eye" type="password" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <MDBBtn color="primary" rounded size="lg">
+                Submit
+              </MDBBtn>
+            </div>
+          </div>
+        </Card>
       </div>
     </div>
   );
